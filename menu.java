d24935ee6988e8fe957b0950a4f8fe6d6706070a
daffafzn <45868357+daffafzn@users.noreply.github.com>
2020-05-21 05:03:06 +0700

    Add files via upload

---
+++ menu.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class menu{
+	public static void main(String[] args){
+	System.out.println(" __  __   ___                       .           ");
+	System.out.println("|  |/  `.'   `.                   .'|           ");
+	System.out.println("|   .-.  .-.   '              .| <  |           ");
+	System.out.println("|  |  |  |  |  |    __      .' |_ | |           ");
+	System.out.println("|  |  |  |  |  | .:--.'.  .'     || | .'''-.    ");
+	System.out.println("|  |  |  |  |  |/ |   | |'--.  .-'| |/.'''. |   ");
+	System.out.println("|  |  |  |  |  |'. __ | |   |  |  |  /    | |  ");
+	System.out.println("|__|  |__|  |__| .'.''| |   |  |  | |     | |  ");
+	System.out.println("                / /   | |_  |  '.'| |     | |  ");
+	System.out.println("                | .._,| '/  |   / | '.    | '. ");
+	System.out.println("                 `--'  `''   `'-'  '---'   '---'");
+	System.out.println("=========================================================");
+	System.out.println("=======================MENU PILIHAN======================");
+	System.out.println("=========================================================");
+	System.out.println("|1| Bangun Datar & Bangun Ruang\t\t\t\t|");
+	System.out.println("|2| Statistika\t\t\t\t\t\t|");
+	System.out.println("|3| Phytagoras\t\t\t\t\t\t|");
+	System.out.println("|4| Kecepatan dan Percepatan\t\t\t\t|");
+	System.out.println("|5| Aritmatika dan Geometri\t\t\t\t|");
+	System.out.println("=========================================================");
+	System.out.println("=========================================================");
+	System.out.println("Masukkan Pilihan : \t\t\t\t\t|");
+	System.out.println("=========================================================");
+
+	}
+}
